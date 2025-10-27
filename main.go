4636d36c42d2660da52f164f3a6a102f7e35f578
bayurifkialghifari <bayurifkialgh@gmail.com>
2025-10-27 16:16:59 +0700

    add errorhandling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"golang-package/database"
 	"golang-package/helper"
@@ -17,4 +18,19 @@ func main() {
 	fmt.Println("Database:", db)
 	fmt.Println("Username:", user)
 	fmt.Println("Password:", pass)
+
+	// Example usage of Bagi function
+	result, err := Bagi(10, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Result:", result)
+	}
+}
+
+func Bagi(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("pembagi tidak boleh nol")
+	}
+	return a / b, nil
 }
